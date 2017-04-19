efec16b1c984719ea4056173d9fb353e88997a80
zhlhuang <364626853@qq.com>
2017-04-19 11:33:47 +0800

    #11 商品类型管理优化

--- Controller/TypeController.class.php
+++ Controller/TypeController.class.php
@@ -5,6 +5,7 @@
 // +----------------------------------------------------------------------
 
 namespace Shop\Controller;
+
 use Common\Controller\AdminBase;
 use Shop\Util\Page;
 
@@ -13,22 +14,22 @@ class TypeController extends AdminBase {
      * 商品类型  用于设置商品的属性
      */
     public function index() {
-        if (IS_POST){
+        if (IS_AJAX) {
             $model = M("GoodsType");
             $count = $model->count();
 
-            $page = I('page',1);
-            $limit = I('limit',10);
-            $page_count = ceil ($count / $limit);
+            $page = I('page', 1);
+            $limit = I('limit', 20);
+            $page_count = ceil($count / $limit);
             $pageArr = array(
                 'page' => $page,
                 'page_count' => $page_count,
             );
-            $res = $model->order("id desc")->page($page,$limit)->select();
+            $res = $model->order("id desc")->page($page, $limit)->select();
             if ($res) {
-                $this->ajaxReturn(['status'=>true, 'data'=>$res, 'page'=>$pageArr]);
+                $this->ajaxReturn(['status' => true, 'data' => $res, 'page' => $pageArr]);
             }
-            $this->ajaxReturn(['status'=>false, 'data'=>[], 'page'=>$pageArr]);
+            $this->ajaxReturn(['status' => false, 'data' => [], 'page' => $pageArr]);
         }
         $this->display('index');
     }
@@ -38,49 +39,49 @@ class TypeController extends AdminBase {
      */
     public function addEditGoodsType() {
         if (IS_POST) {
-            $id = I('id',0);
+            $id = I('id', 0);
             $post = I('post.');
             $model = M("GoodsType");
 
             if ($id == '') {
                 $res = $model->add($post);
-                $this->ajaxReturn(['data'=>$post,'status'=>true]);
+                $this->ajaxReturn(['data' => $post, 'status' => true]);
             } else {
                 unset($post['id']);
-                $res = $model->where(['id'=>$id])->save($post);
-                $this->ajaxReturn(['data'=>$res,'status'=>true]);
+                $res = $model->where(['id' => $id])->save($post);
+                $this->ajaxReturn(['data' => $res, 'status' => true]);
             }
         }
 
     }
+
     public function getTypeDetail() {
         $id = I('id', 0);
-        if (IS_POST) {
+        if (IS_AJAX) {
             $res = M("GoodsType")->find($id);
             if ($res) {
                 $this->ajaxReturn(['data' => $res, 'status' => true]);
             } else {
-                $this->ajaxReturn(['data' => ['name'=>'','id'=>$id], 'status' => false]);
+                $this->ajaxReturn(['data' => ['name' => '', 'id' => $id], 'status' => false]);
             }
         }
-        $this->assign('id',$id);
+        $this->assign('id', $id);
         $this->display('goods_type');
     }
 
-     /**
-     * 删除商品类型 
+    /**
+     * 删除商品类型
      */
-    public function delGoodsType()
-    {
-        $id = I('id','');
+    public function delGoodsType() {
+        $id = I('post.id', 0);
         // 判断 商品规格        
-        $count = M("Spec")->where(['type_id'=>$id])->count("1");
-        $count > 0 && $this->error('该类型下有商品规格不得删除!',U('Type/index'));
+        $count = M("Spec")->where(['type_id' => $id])->count("1");
+        $count > 0 && $this->error('该类型下有商品规格不得删除!', U('Type/index'));
         // 判断 商品属性        
-        $count = M("GoodsAttribute")->where(['type_id'=>$id])->count("1");
-        $count > 0 && $this->error('该类型下有商品属性不得删除!',U('Type/index'));        
+        $count = M("GoodsAttribute")->where(['type_id' => $id])->count("1");
+        $count > 0 && $this->error('该类型下有商品属性不得删除!', U('Type/index'));
         // 删除分类
-        M('GoodsType')->where(['id'=>$id])->delete();
-        $this->success("操作成功",U('Type/index'));
-    }    
+        M('GoodsType')->where(['id' => $id])->delete();
+        $this->success("操作成功", U('Type/index'));
+    }
 }
--- View/Type/index.php
+++ View/Type/index.php
@@ -1,62 +1,59 @@
 <include file="Public/min-header"/>
 <div class="wrapper">
-  <include file="Public/breadcrumb"/>
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title"><i class="fa fa-list"></i> 商品类型列表</h3>
-        </div>
-        <div class="panel-body">    
-		<div class="navbar navbar-default">
-            <div class="row navbar-form">
-                <button type="submit" onclick="location.href='{:U('Type/getTypeDetail')}'"  class="btn btn-primary pull-right"><i class="fa fa-plus"></i>新增商品类型</button>
-            </div>
-          </div>
-                        
-          <div id="app">
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th class="sorting text-center">ID</th>                                
-                                <th class="sorting text-center">类型名</th>
-                                <th class="sorting text-center">操作</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="item in lists">
-                                <td class="text-center">{{ item.id }}</td>
-                                <td class="text-center">{{ item.name }}</td>
-                                <td class="text-center">
-                                    <a :href="'{:U('Spec/index')}&type_id='+item.id" data-toggle="tooltip" title="" class="btn btn-info" data-original-title="属性列表"><i class="fa fa-eye"></i></a>
-                                    <a :href="'{:U('Type/getTypeDetail')}&id='+item.id" data-toggle="tooltip" title="" class="btn btn-primary" data-original-title="编辑"><i class="fa fa-pencil"></i></a>
-                                    <a href="javascript:;" v-on:click="delGoodsType(item.id)" data-toggle="tooltip" title="" class="btn btn-danger" data-original-title="删除"><i class="fa fa-trash-o"></i></a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
+    <include file="Public/breadcrumb"/>
+    <!-- Main content -->
+    <section class="content" id="app">
+        <div class="container-fluid">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-list"></i> 商品类型列表</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="navbar navbar-default">
+                        <div class="row navbar-form">
+                            <button type="submit" @click="addBtn"
+                                    class="btn btn-primary pull-right"><i class="fa fa-plus"></i>新增商品类型
+                            </button>
+                        </div>
                     </div>
-              <div class="dataTables_paginate paging_simple_numbers">
-                  <button v-on:click="toPage( parseInt(page) - 1 )" class="btn btn-primary">上一页
-                  </button>
-                  <button v-on:click="toPage( parseInt(page) + 1 )" class="btn btn-primary">下一页
-                  </button>
-                  <span style="line-height: 30px;margin-left: 50px"><input id="ipt_page"
-                                                                           style="width:30px;"
-                                                                           type="text"
-                                                                           v-model="temp_page"> / {{ page_count }}</span>
-                  <span><button class="btn btn-primary"
-                                v-on:click="toPage( temp_page )">GO</button></span>
-              </div>
-          </div>
+                    <div>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th class="sorting text-center">ID</th>
+                                    <th class="sorting text-center">类型名</th>
+                                    <th class="sorting text-center">操作</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="item in lists">
+                                    <td class="text-center">{{ item.id }}</td>
+                                    <td class="text-center">{{ item.name }}</td>
+                                    <td class="text-center">
+                                        <a :href="'{:U('Spec/index')}&type_id='+item.id" data-toggle="tooltip" title=""
+                                           class="btn btn-info" data-original-title="属性列表"><i class="fa fa-eye"></i></a>
+                                        <a href="javascript:;" @click="editBtn(item.id)" data-toggle="tooltip"
+                                           title="" class="btn btn-primary" data-original-title="编辑"><i
+                                                    class="fa fa-pencil"></i></a>
+                                        <a href="javascript:;" v-on:click="delGoodsType(item.id)" data-toggle="tooltip"
+                                           title="" class="btn btn-danger" data-original-title="删除"><i
+                                                    class="fa fa-trash-o"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!--     分页-->
+                        <v-page :page="page" v-on:update="getList" :page_count="page_count"></v-page>
+                        <!--   /分页-->
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <!-- /.row --> 
-  </section>
-  <!-- /.content --> 
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 <include file="Public/vue"/>
@@ -64,38 +61,64 @@
     new Vue({
         el: '#app',
         data: {
-            lists:[],
+            lists: [],
             page: 1,
             page_count: 1,
-            temp_page: 1
         },
+        mixins: [window.__baseMethods],
         methods: {
-            getList: function(){
-                var that = this;
-                $.ajax({url: '', type: 'post', data: {'page': that.page}, dataType: 'json',
-                    success:function (res){
-                        if(res.status){
-                            that.lists = res.data;
-                            that.page = res.page['page'];
-                            that.temp_page = res.page['page'];
-                            that.page_count = res.page['page_count'];
-                        }
+            editBtn: function (id) {
+                var that = this
+                open_window = layer.open({
+                    title: '编辑类型',
+                    type: 2,
+                    area: ['800px', '330px'],
+                    fixed: false, //不固定
+                    maxmin: false,
+                    content: "{:U('Type/getTypeDetail')}&id=" + id,
+                    end: function () {
+                        that.getList()
                     }
                 });
             },
+            addBtn: function () {
+                var that = this
+                open_window = layer.open({
+                    title: '添加类型',
+                    type: 2,
+                    area: ['800px', '330px'],
+                    fixed: false, //不固定
+                    maxmin: false,
+                    content: "{:U('Type/getTypeDetail')}",
+                    end: function () {
+                        that.getList()
+                    }
+                });
+            },
+            getList: function () {
+                var that = this;
+                var where = {'page': that.page}
+                that.httpGet("{:U('Type/index')}", where, function (res) {
+                    if (res.status) {
+                        that.lists = res.data;
+                        that.page = res.page['page'];
+                        that.page_count = res.page['page_count'];
+                    }
+                }, false)
+            },
             delGoodsType: function (id) {
-                layer.confirm('确定要删除该分类吗？',{
-                    btn:['确定', '取消']
-                },function () {
-                    $.ajax({url: "{:U('Type/delGoodsType')}", type: 'get', data: {'id': id}, dataType: 'json',
-                        success:function (res){
-                            layer.alert(res.info,{
-                                icon: res.status
-                            },function(){
-                                window.location.reload();
-                            });
-                        }
-                    });
+                var that = this
+                layer.confirm('确定要删除该分类吗？', {
+                    btn: ['确定', '取消']
+                }, function () {
+                    that.httpPost("{:U('Type/delGoodsType')}", {'id': id}, function (res) {
+                        layer.alert(res.info, {
+                            icon: res.status
+                        }, function () {
+                            layer.closeAll()
+                            that.getList()
+                        });
+                    })
                 });
             },
             toPage: function (page) {
@@ -110,8 +133,11 @@
                 this.getList();
             }
         },
-        mounted: function(){
+        mounted: function () {
             this.getList();
+        },
+        components: {
+            'v-page': pageComponent
         }
     });
 </script>
