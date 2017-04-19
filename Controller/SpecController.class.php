f292ef90c5910b49cb774d9f7ec5840f89f642e3
zhlhuang <364626853@qq.com>
2017-04-19 12:19:18 +0800

    #11 商品规格后台管理

--- Controller/SpecController.class.php
+++ Controller/SpecController.class.php
@@ -5,6 +5,7 @@
 // +----------------------------------------------------------------------
 
 namespace Shop\Controller;
+
 use Common\Controller\AdminBase;
 use Shop\Util\Page;
 use Shop\Logic\GoodsLogic;
@@ -15,7 +16,7 @@ class SpecController extends AdminBase {
      * 商品类型  用于设置商品的属性
      */
     public function index() {
-        if (IS_POST) {
+        if (IS_AJAX) {
             $data = $this->getSpecList();
             $this->ajaxReturn($data);
         }
@@ -44,103 +45,112 @@ class SpecController extends AdminBase {
         $this->assign('goodsType', $goodsType);
         $this->display('goods_type');
     }
-     /**
-     * 删除商品类型 
+
+    /**
+     * 删除商品类型
      */
-    public function delGoodsType()
-    {
+    public function delGoodsType() {
         // 判断 商品规格        
-        $count = M("Spec")->where("type_id = {$_GET['id']}")->count("1");   
-        $count > 0 && $this->error('该类型下有商品规格不得删除!',U('Type/index'));
+        $count = M("Spec")->where("type_id = {$_GET['id']}")->count("1");
+        $count > 0 && $this->error('该类型下有商品规格不得删除!', U('Type/index'));
         // 判断 商品属性        
-        $count = M("GoodsAttribute")->where("type_id = {$_GET['id']}")->count("1");   
-        $count > 0 && $this->error('该类型下有商品属性不得删除!',U('Type/index'));        
+        $count = M("GoodsAttribute")->where("type_id = {$_GET['id']}")->count("1");
+        $count > 0 && $this->error('该类型下有商品属性不得删除!', U('Type/index'));
         // 删除分类
-        M('GoodsType')->where("id = {$_GET['id']}")->delete();   
-        $this->success("操作成功",U('Type/index'));
+        M('GoodsType')->where("id = {$_GET['id']}")->delete();
+        $this->success("操作成功", U('Type/index'));
     }
-     /**
+
+    /**
      *  商品规格列表
      */
-    public function getSpecList(){
+    public function getSpecList() {
 
         $where = ' 1 = 1 '; // 搜索条件                        
-        I('type_id')   && $where = "$where and type_id = ".I('type_id') ;        
+        I('type_id') && $where = "$where and type_id = " . I('type_id');
         // 关键词搜索               
         $model = D('spec');
         $count = $model->where($where)->count();
-        $page = I('page',1);
-        $limit = I('limit',10);
-        $page_count = ceil ($count / $limit);
+        $page = I('page', 1);
+        $limit = I('limit', 20);
+        $page_count = ceil($count / $limit);
         $pageArr = array(
             'page' => $page,
             'page_count' => $page_count,
         );
 
-        $specList = $model->where($where)->order('`type_id` asc')->page($page,$limit)->select();
-        $GoodsLogic = new GoodsLogic();        
-        foreach($specList as $k => $v)
-        {       // 获取规格项     
-                $arr = $GoodsLogic->getSpecItem($v['id']);
-                $specList[$k]['spec_item'] = implode(' , ', $arr);
+        $specList = $model->where($where)->order('`order` desc')->page($page, $limit)->select();
+        $GoodsLogic = new GoodsLogic();
+        foreach ($specList as $k => $v) {       // 获取规格项
+            $arr = $GoodsLogic->getSpecItem($v['id']);
+            $specList[$k]['spec_item'] = implode(' , ', $arr);
         }
-        
+
 
         $goodsTypeList = M("GoodsType")->select(); // 规格分类
         $goodsTypeList = convert_arr_key($goodsTypeList, 'id');
 
-        return ['goodsTypeList'=>$goodsTypeList,'specList'=>$specList, 'page'=>$pageArr];
+        return ['goodsTypeList' => $goodsTypeList, 'specList' => $specList, 'page' => $pageArr];
     }
-     /**
+
+    /**
      * 添加修改编辑  商品规格
      */
-    public  function addEditSpec(){
+    public function addEditSpec() {
         if (IS_POST) {
             $post = I('post.');
 
-            if($post['detail']['type_id'] == ''){
-                $this->ajaxReturn(['msg'=>'没有选择商品类型','status'=>false]);
+            if ($post['detail']['type_id'] == '') {
+                $this->ajaxReturn(['msg' => '没有选择商品类型', 'status' => false]);
             }
 
             $items = $post['items'];
             $itemsArr = explode('
-',$items);
-
+', $items);
             $model = M('Spec');
-            $id = I('id',0);
-
+            $id = I('id', 0);
             if ($id == 0) {
                 // 添加
                 $res = $model->add($post['detail']); // 插入id
-                foreach($itemsArr as $val){
-                    M('specItem')->add(['spec_id'=>$res, 'item'=>$val]);
+                foreach ($itemsArr as $val) {
+                    if ($val != '') {
+                        M('specItem')->add(['spec_id' => $res, 'item' => $val]);
+                    }
                 }
             } else {
                 // 修改
-                $res = $model->where(['id'=>$id])->save($post['detail']);
-                M('specItem')->where(['spec_id'=>$id])->delete();
-                foreach($itemsArr as $val){
-                    M('specItem')->add(['spec_id'=>$id, 'item'=>$val]);
+                $res = $model->where(['id' => $id])->save($post['detail']);
+                M('specItem')->where(['spec_id' => $id])->delete();
+                foreach ($itemsArr as $val) {
+                    if ($val != '') {
+                        M('specItem')->add(['spec_id' => $id, 'item' => $val]);
+                    }
                 }
             }
-            $this->ajaxReturn(['data'=>$res,'status'=>true]);
+            $this->ajaxReturn(['data' => $res, 'status' => true]);
 
         }
     }
+
     public function getSpecDetail() {
         $id = I('id', 0);
-        if (IS_POST) {
+        if (IS_AJAX) {
             $res = M("Spec")->find($id);
             $goodsType = M("GoodsType")->select();
             if ($res) {
-                $specItem = M('SpecItem')->where(['spec_id'=>$id])->select();
+                $specItem = M('SpecItem')->where(['spec_id' => $id])->select();
                 $specItemStr = '';
-                foreach($specItem as $val){
-                    $specItemStr .= $val['item']."
+                foreach ($specItem as $val) {
+                    $specItemStr .= $val['item'] . "
 ";
                 }
                 $specItemStr = rtrim($specItemStr);
-                $this->ajaxReturn(['specItemStr'=>$specItemStr,'goodsType'=>$goodsType,'data' => $res, 'status' => true]);
+                $this->ajaxReturn([
+                    'specItemStr' => $specItemStr,
+                    'goodsType' => $goodsType,
+                    'data' => $res,
+                    'status' => true
+                ]);
             } else {
                 $res = [
                     'type_id' => '',
@@ -148,22 +158,23 @@ class SpecController extends AdminBase {
                     'order' => '',
                     'search_index' => ''
                 ];
-                $this->ajaxReturn(['goodsType'=>$goodsType,'data' => $res, 'status' => false]);
+                $this->ajaxReturn(['goodsType' => $goodsType, 'data' => $res, 'status' => false]);
             }
         }
-        $this->assign('id',$id);
+        $this->assign('id', $id);
         $this->display('spec');
     }
-      /**
+
+    /**
      * 删除商品规格
      */
-    public function delGoodsSpec()
-    {
+    public function delGoodsSpec() {
+        $id = I('post.id');
         // 判断 商品规格项
-        $count = M("SpecItem")->where("spec_id = {$_GET['id']}")->count("1");   
-        $count > 0 && $this->error('清空规格项后才可以删除!',U('Spec/index'));
+        $count = M("SpecItem")->where("spec_id = '%d'", $id)->count("1");
+        $count > 0 && $this->error('清空规格项后才可以删除!', U('Spec/index'));
         // 删除分类
-        M('Spec')->where("id = {$_GET['id']}")->delete();   
-        $this->success("删除成功",U('Spec/index'));
-    }     
+        M('Spec')->where("id = '%d'", $id)->delete();
+        $this->success("删除成功", U('Spec/index'));
+    }
 }
--- View/Spec/index.php
+++ View/Spec/index.php
@@ -1,87 +1,81 @@
 <include file="Public/min-header"/>
-<div class="wrapper"> 
-  <include file="Public/breadcrumb"/>
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title"><i class="fa fa-list"></i> 商品规格</h3>
-        </div>
-        <div class="panel-body" id="app">
-          <div class="navbar navbar-default">
-              <form action="" id="search-form2" class="navbar-form form-inline" method="post" onsubmit="return false">
-                <div class="form-group">
-                  <select name="type_id" id="type_id" class="form-control">
-                      <option value="">所有分类</option>
-                      <option v-for="item in goodsTypeList" :value="item.id">{{item.name}}</option>
-                   </select>
+<div class="wrapper">
+    <include file="Public/breadcrumb"/>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-list"></i> 商品规格</h3>
                 </div>
-                <div class="form-group">
-	                <button type="submit" v-on:click="filter()" id="button-filter" class="btn btn-primary pull-right">
-	                 <i class="fa fa-search"></i> 筛选
-	                </button>
-                </div> 
-                <button type="submit" onclick="location.href='{:U('Spec/getSpecDetail')}'" id="button-filter2" class="btn btn-primary pull-right">
-                 <i class="fa fa-plus"></i> 添加规格
-                </button>                                 
-              </form>
+                <div class="panel-body" id="app">
+                    <div class="navbar navbar-default">
+                        <form action="" id="search-form2" class="navbar-form form-inline" method="post"
+                              onsubmit="return false">
+                            <div class="form-group">
+                                <select @change="filter" name="type_id" id="type_id" class="form-control">
+                                    <option value="">所有分类</option>
+                                    <option v-for="item in goodsTypeList" :value="item.id">{{item.name}}</option>
+                                </select>
+                            </div>
+                            <button type="submit" @click="addBtn" id="button-filter2"
+                                    class="btn btn-primary pull-right">
+                                <i class="fa fa-plus"></i> 添加规格
+                            </button>
+                        </form>
 
-          </div>
-          <div id="ajax_return">
-              <form method="post" enctype="multipart/form-data" target="_blank" id="form-goodsType">
-                  <div class="table-responsive">
-                      <table class="table table-bordered table-hover">
-                          <thead>
-                          <tr>
-                              <th class="sorting text-left">ID</th>
-                              <th class="sorting text-left">规格类型</th>
-                              <th class="sorting text-left">规格名称</th>
-                              <th class="sorting text-left">规格项</th>
-                              <th class="sorting text-center">筛选</th>
-                              <th class="sorting text-left">排序</th>
-                              <th class="sorting text-left">操作</th>
-                          </tr>
-                          </thead>
-                          <tbody>
-                          <tr v-for="item in specList">
-                              <td class="text-right">{{item.id}}</td>
-                              <td class="text-left">{{ goodsTypeList[item.type_id]['name'] }}</td>
-                              <td class="text-left">{{item.name}}</td>
-                              <td class="text-left">{{item.spec_item}}</td>
-                              <td class="text-center">
-                                  <img v-on:click="changeSearchIndex(item)" width="20" height="20" v-bind:src="item.search_index == 1 ? '{$config_siteurl}statics/extres/shop/images/yes.png' : '{$config_siteurl}statics/extres/shop/images/cancel.png'"/>
-                              </td>
-                              <td class="text-right">
-                                  <input type="text" class="form-control input-sm" v-on:change="updateOrder(item)" size="4" v-model="item.order" />
-                              </td>
-                              <td class="text-left">
-                                  <a :href="'{:U('Spec/getSpecDetail')}&id='+item.id" data-toggle="tooltip" title="" class="btn btn-primary" data-original-title="编辑"><i class="fa fa-pencil"></i></a>
-                                  <a v-on:click="delGoodsSpec(item.id)" href="javascript:;" id="button-delete6" data-toggle="tooltip" title="" class="btn btn-danger" data-original-title="删除"><i class="fa fa-trash-o"></i></a></td>
-                          </tr>
-                          </tbody>
-                      </table>
-                  </div>
-              </form>
-              <div class="dataTables_paginate paging_simple_numbers">
-                  <button v-on:click="toPage( parseInt(page) - 1 )" class="btn btn-primary">上一页
-                  </button>
-                  <button v-on:click="toPage( parseInt(page) + 1 )" class="btn btn-primary">下一页
-                  </button>
-                  <span style="line-height: 30px;margin-left: 50px"><input id="ipt_page"
-                                                                           style="width:30px;"
-                                                                           type="text"
-                                                                           v-model="temp_page"> / {{ page_count }}</span>
-                  <span><button class="btn btn-primary"
-                                v-on:click="toPage( temp_page )">GO</button></span>
-              </div>
-          </div>
+                    </div>
+                    <div id="ajax_return">
+                        <form method="post" enctype="multipart/form-data" target="_blank" id="form-goodsType">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="sorting text-left">ID</th>
+                                        <th class="sorting text-left">规格类型</th>
+                                        <th class="sorting text-left">规格名称</th>
+                                        <th class="sorting text-left">规格项</th>
+                                        <th class="sorting text-center">筛选</th>
+                                        <th class="sorting text-left">排序</th>
+                                        <th class="sorting text-left">操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="item in specList">
+                                        <td class="text-right">{{item.id}}</td>
+                                        <td class="text-left">{{ goodsTypeList[item.type_id]['name'] }}</td>
+                                        <td class="text-left">{{item.name}}</td>
+                                        <td class="text-left">{{item.spec_item}}</td>
+                                        <td class="text-center">
+                                            <img v-on:click="changeSearchIndex(item)" width="20" height="20"
+                                                 v-bind:src="item.search_index == 1 ? '{$config_siteurl}statics/extres/shop/images/yes.png' : '{$config_siteurl}statics/extres/shop/images/cancel.png'"/>
+                                        </td>
+                                        <td class="text-right">
+                                            <input type="text" class="form-control input-sm"
+                                                   v-on:change="updateOrder(item)" size="4" v-model="item.order"/>
+                                        </td>
+                                        <td class="text-left">
+                                            <a href="javascript:;" @click="editBtn(item.id)" data-toggle="tooltip"
+                                               title="" class="btn btn-primary" data-original-title="编辑"><i
+                                                        class="fa fa-pencil"></i></a>
+                                            <a v-on:click="delGoodsSpec(item.id)" href="javascript:;"
+                                               id="button-delete6" data-toggle="tooltip" title="" class="btn btn-danger"
+                                               data-original-title="删除"><i class="fa fa-trash-o"></i></a></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </form>
+                        <!--     分页-->
+                        <v-page :page="page" v-on:update="getList" :page_count="page_count"></v-page>
+                        <!--   /分页-->
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <!-- /.row --> 
-  </section>
-  <!-- /.content --> 
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 <include file="Public/vue"/>
@@ -94,79 +88,83 @@
             specList: [],
             page: 1,
             page_count: 1,
-            temp_page: 1
         },
+        mixins: [window.__baseMethods],
         methods: {
-            getList: function(){
-                var that = this;
-                $.ajax({
-                    url: '',
-                    type: 'post',
-                    data: that.where,
-                    dataType: 'json',
-                    success: function(res){
-                        that.goodsTypeList = res.goodsTypeList;
-                        that.specList = res.specList;
-                        that.page = res.page['page'];
-                        that.temp_page = res.page['page'];
-                        that.page_count = res.page['page_count'];
+            editBtn: function (id) {
+                var that = this
+                layer.open({
+                    title: '编辑规格',
+                    type: 2,
+                    area: ['800px', '630px'],
+                    fixed: false, //不固定
+                    maxmin: false,
+                    content: "{:U('Spec/getSpecDetail')}&id=" + id,
+                    end: function () {
+                        that.getList()
                     }
                 });
+            },
+            addBtn: function () {
+                var that = this
+                layer.open({
+                    title: '编辑规格',
+                    type: 2,
+                    area: ['800px', '630px'],
+                    fixed: false, //不固定
+                    maxmin: false,
+                    content: "{:U('Spec/getSpecDetail')}",
+                    end: function () {
+                        that.getList()
+                    }
+                });
+            },
+            getList: function () {
+                var that = this;
+                that.where.page = that.page
+                that.httpGet("{:U('Spec/index')}", that.where, function (res) {
+                    that.goodsTypeList = res.goodsTypeList;
+                    that.specList = res.specList;
+                    that.page = res.page['page'];
+                    that.page_count = res.page['page_count'];
+                    if (that.where.type_id) {
+                        setTimeout(function () {
+                            $('#type_id').val(that.where.type_id)
+                        }, 300)
+                    }
+                }, false)
             },
             filter: function () {
                 var type_id = $('#type_id').val();
                 this.where = {'type_id': type_id};
                 this.getList();
             },
-            changeSearchIndex: function(obj){
-                if (obj.search_index == 1){
+            changeSearchIndex: function (obj) {
+                if (obj.search_index == 1) {
                     obj.search_index = 0;
                 } else {
                     obj.search_index = 1;
                 }
-
-                $.ajax({
-                    url: "{:U('Shop/AdminApi/changeTableVal')}",
-                    data: {
-                        'table': 'spec',
-                        'id_name': 'id',
-                        'id_value': obj.id,
-                        'field': 'search_index',
-                        'value': obj.search_index
-                    },
-                    success: function(res){
-                        layer.msg('操作成功');
-                    }
-                });
+                var that = this
+                that.changeTableVal('spec', 'id', obj.id, 'search_index', obj.search_index)
             },
             updateOrder: function (obj) {
-                $.ajax({
-                    url: "{:U('Shop/AdminApi/changeTableVal')}",
-                    data: {
-                        'table': 'spec',
-                        'id_name': 'id',
-                        'id_value': obj.id,
-                        'field': 'order',
-                        'value': obj.order
-                    },
-                    success: function(res){
-                        layer.msg('操作成功');
-                    }
-                });
+                var that = this
+                that.changeTableVal('spec', 'id', obj.id, 'order', obj.order)
             },
             delGoodsSpec: function (id) {
-                layer.confirm('确定要删除该规格吗？',{
-                    btn:['确定', '取消']
-                },function () {
-                    $.ajax({url: "{:U('Spec/delGoodsSpec')}", type: 'get', data: {'id': id}, dataType: 'json',
-                        success:function (res){
-                            layer.alert(res.info,{
-                                icon: res.status
-                            },function(){
-                                window.location.reload();
-                            });
-                        }
-                    });
+                var that = this
+                layer.confirm('确定要删除该规格吗？', {
+                    btn: ['确定', '取消']
+                }, function () {
+                    that.httpPost("{:U('Spec/delGoodsSpec')}", {'id': id}, function (res) {
+                        layer.alert(res.info, {
+                            icon: res.status
+                        }, function () {
+                            layer.closeAll()
+                            that.getList()
+                        });
+                    })
                 });
             },
             toPage: function (page) {
@@ -181,11 +179,21 @@
                 this.getList();
             }
         },
-        mounted: function(){
+        mounted: function () {
+            var that = this
+            var where = {}
+            if (that.getQueryString('type_id')) {
+                var type_id = that.getQueryString('type_id')
+                where['type_id'] = type_id
+            }
+            this.where = where
             this.getList();
+        },
+        components: {
+            'v-page': pageComponent
         }
     });
-	 
-</script> 
+
+</script>
 </body>
 </html>
